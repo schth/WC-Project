79f9ecd567f0faa245ab388e79f48a71c9702aa2
tatsutatsugo <doubledragontaz@gmail.com>
2017-11-12 13:20:58 +0900

    Beautify files

--- prototype/IoTGatewayprototype.py
+++ prototype/IoTGatewayprototype.py
@@ -1,5 +1,5 @@
 # coding: UTF-8
-#pySerialのInstallが必要
+# pySerialのInstallが必要
 import serial
 import datetime
 import urllib
@@ -8,15 +8,15 @@ import requests
 # COM5を開く windows用
 s = serial.Serial("COM5", 115200)
 
-#Mac用
+# Mac用
 #s = serial.Serial("/dev/tty.usbserial-MW1IQ8BN", 115200)
 
-#センサーの閾値
+# センサーの閾値
 sensor = 1000
 
-room1DiviceId="10e27d0"
-room1CurrentStatus=False
-room1BeforeStatus=False
+room1DiviceId = "10e27d0"
+room1CurrentStatus = False
+room1BeforeStatus = False
 
 while 1:
     # 1行読み取る
@@ -24,31 +24,33 @@ while 1:
     # 「;」で分割する
     m = str(data).split(";")
     if (len(m) == 13):
-        if m[5]=="10e27d0":
+        if m[5] == "10e27d0":
             if int(m[9]) < sensor:
                 room1CurrentStatus = False
                 if room1BeforeStatus != room1CurrentStatus:
                     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %I:%M:%S ")
-                    print(timestamp +" "+m[5]+": is opened !")
+                    print(timestamp + " " + m[5] + ": is opened !")
                     # 取得した値を変数に格納
                     comp_id = 1  # ここをusbから取得した値にしたい
                     status = 'N'  # ここをusbから取得した値にしたい
                     wc_status = {'g_id': comp_id, 'g_status': status}
                     # httpプロトコルでpost通信
-                    response = requests.post('http://localhost/wc_get.php', params=wc_status)
+                    response = requests.post(
+                        'http://localhost/wc_get.php', params=wc_status)
                     print(response.text)  # レスポンスのHTMLを文字列で取得
                     room1BeforeStatus = room1CurrentStatus
             elif int(m[9]) > sensor:
                 room1CurrentStatus = True
                 if room1BeforeStatus != room1CurrentStatus:
                     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %I:%M:%S")
-                    print(timestamp +" "+m[5]+": is closed !")
+                    print(timestamp + " " + m[5] + ": is closed !")
                     # 取得した値を変数に格納
                     comp_id = 1  # ここをusbから取得した値にしたい
                     status = 'Y'  # ここをusbから取得した値にしたい
                     wc_status = {'g_id': comp_id, 'g_status': status}
                     # httpプロトコルでpost通信
-                    response = requests.post('http://localhost/wc_get.php', params=wc_status)
+                    response = requests.post(
+                        'http://localhost/wc_get.php', params=wc_status)
                     print(response.text)  # レスポンスのHTMLを文字列で取得
                     room1BeforeStatus = room1CurrentStatus
         #print ("送信元シリアル番号："+m[5]+" / 送信元電源電圧："+m[6]+" / センサー電流："+m[9])
--- prototype/wc_get.php
+++ prototype/wc_get.php
@@ -1,18 +1,18 @@
 <?php
     //データベースへの接続
-    $link = mysqli_connect("127.0.0.1","root","","wc");
-    if(mysqli_connect_error()){
+    $link = mysqli_connect("127.0.0.1", "root", "", "wc");
+    if (mysqli_connect_error()) {
         die("データベースへの接続に失敗しました。");
     }
     //登録データを変数に格納
-    $comp_id = $_GET['g_id']; 
+    $comp_id = $_GET['g_id'];
     $status = $_GET['g_status'];
     date_default_timezone_set('Asia/Tokyo');
     $today = date("Y-m-d H:i:s");
     //データベースにデータを登録
     $query = "INSERT INTO `components` (`comp_id`,`status`,`upd_dt`) VALUES ('$comp_id','$status','$today')";
-    
-    if($result = mysqli_query($link,$query)){
+
+    if ($result = mysqli_query($link, $query)) {
         echo "INSERTクエリの発行に成功しました";
     }
     //$query = "SELECT * FROM components";
@@ -22,5 +22,4 @@
     //$row = mysqli_fetch_array($result);
     //echo "<p>";
     //echo "トイレIDは".$row['comp_ID']."、利用状況は".$row['status']."です。";
-
 ?>
