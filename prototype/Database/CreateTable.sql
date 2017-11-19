a0fc53530e33717992b9e5df7c496d242a64723f
schth <scht26@gmail.com>
2017-11-19 18:14:12 +0900

    テーブル名修正、wc-adminにROOT権限、wcusrに実行権限を付与

--- prototype/Database/CreateTable.sql
+++ prototype/Database/CreateTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE wc (
+CREATE TABLE compartment (
   comp_id integer(1), 
   status varchar(1),
   upd_dt time,
--- prototype/Database/GrantPrivileges.sql
+++ prototype/Database/GrantPrivileges.sql
@@ -1,2 +1,3 @@
-GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.compartment TO 'wcsys'@'localhost';
+GRANT ALL ON `wc`.compartment TO 'wc-admin'@'localhost';
+GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.compartment TO 'wcusr'@'localhost';
 GRANT SELECT ON `wc`.compartment TO 'wcses'@'localhost';
