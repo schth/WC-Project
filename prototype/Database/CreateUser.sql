5547323d2dd0275d6bd361d10d6e65cb1114df7a
tatsutatsugo <doubledragontaz@gmail.com>
2017-11-23 15:00:56 +0900

    beatify files;

--- prototype/Database/CreateUser.sql
+++ prototype/Database/CreateUser.sql
@@ -1,3 +1,3 @@
-CREATE USER
-'wc-admin'@'localhost' IDENTIFIED BY 'wc-admin',
-'wcses'@'localhost' IDENTIFIED BY 'wcses';
+CREATE USER 'wc-admin'@'localhost' IDENTIFIED BY 'wc-admin',
+                                                 'wcses'@'localhost' IDENTIFIED BY 'wcses';
+
--- prototype/Database/GrantPrivileges.sql
+++ prototype/Database/GrantPrivileges.sql
@@ -1,3 +1,10 @@
 GRANT ALL ON `wc`.compartment TO 'wc-admin'@'localhost';
-GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.compartment TO 'wcusr'@'localhost';
-GRANT SELECT ON `wc`.compartment TO 'wcses'@'localhost';
+
+GRANT
+SELECT,
+UPDATE,
+INSERT,
+DELETE ON `wc`.compartment TO 'wcusr'@'localhost';
+
+GRANT
+SELECT ON `wc`.compartment TO 'wcses'@'localhost';
