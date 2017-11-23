a53cfc835b3cb9660e062eb47f4810c99c0cea7f
tatsutatsugo <doubledragontaz@gmail.com>
2017-11-23 15:15:40 +0900

    beautify files

--- prototype/Database/CreateTable.sql
+++ prototype/Database/CreateTable.sql
@@ -1,6 +1,6 @@
-CREATE TABLE compartment ( comp_id integer(1),
-                                   status varchar(1),
-                                          upd_dt TIME,
-                                                 PRIMARY KEY (comp_id,
-                                                              upd_dt));
-
+CREATE TABLE compartment (
+  comp_id integer(1),
+  status varchar(1),
+  upd_dt TIME,
+  PRIMARY KEY (comp_id,
+               upd_dt));
--- prototype/Database/GrantPrivileges.sql
+++ prototype/Database/GrantPrivileges.sql
@@ -1,10 +1,3 @@
 GRANT ALL ON `wc`.compartment TO 'wc-admin'@'localhost';
-
-GRANT
-SELECT,
-UPDATE,
-INSERT,
-DELETE ON `wc`.compartment TO 'wcusr'@'localhost';
-
-GRANT
-SELECT ON `wc`.compartment TO 'wcses'@'localhost';
+GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.compartment TO 'wcusr'@'localhost';
+GRANT SELECT ON `wc`.compartment TO 'wcses'@'localhost';
