f0d2624aa55f574561737b210c7f2ec9e336ad22
tatsutatsugo <doubledragontaz@gmail.com>
2017-11-28 22:38:42 +0900

    modify bug

--- prototype/html/index.php
+++ prototype/html/index.php
@@ -19,7 +19,7 @@
       <p>
        <?php
           // APIを利用
-          $url = 'http://127.0.0.1/api/compartment/read.php';
+          $url = 'http://127.0.0.1/api/compartment/read_status.php';
           $json = file_get_contents($url);
           // $json文字列をオブジェクト型に変換
           $result = json_decode($json);
