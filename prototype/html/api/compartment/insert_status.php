2125d006eab01efeac251edce5c067503d2d26c3
schth <scht26@gmail.com>
2017-11-30 23:23:30 +0900

    create API as insert_status

--- prototype/html/api/compartment/insert_status.php
+++ prototype/html/api/compartment/insert_status.php
@@ -1,7 +1,7 @@
 <?php
 // ファイル読み込み
-include_once 'api/config/database.php';
-include_once 'insert.php';
+include_once '../config/database.php';
+include_once '../objects/compartment.php';
 
 // Databaseインスタンス生成
 $database = new Database();
@@ -9,13 +9,13 @@ $database = new Database();
 $db = $database->db_connect();
 
 // 初期化
-$get_stat = new get_stat($db);
+$compartment = new compartment($db);
 
 //GETメソッドでセンサーから送信された値を取得し、変数に格納
 $comp_id = $_GET['g_id'];
 $status = $_GET['g_status'];
 
 //get_statクラスのinsert関数をセンサーの値を引数にして実行
-$get_stat->insert($comp_id,$status);
+$compartment->insert($comp_id,$status);
 
 ?>
--- prototype/html/api/objects/compartment.php
+++ prototype/html/api/objects/compartment.php
@@ -32,6 +32,17 @@ class Compartment
 
         return $stmt;
     }
+    
+     public function insert($comp_id,$status)
+    {
+        //現在日付時刻を変数に格納
+        date_default_timezone_set('Asia/Tokyo');
+        $today = date("Y-m-d H:i:s");
+        //データベースにデータを登録
+        $query = "INSERT INTO `compartment` (`comp_id`,`status`,`upd_dt`) VALUES ('$comp_id','$status','$today')";
+        
+        mysqli_query($this->conn, $query);
+    }
 }
 
 ?>
