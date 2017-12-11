ea7159c2422464e011bd3f3fd00e560f85ee0154
tatsutatsugo <doubledragontaz@gmail.com>
2017-12-11 23:37:15 +0900

    modify http post address

--- prototype/IoTGateway/IoTGatewayprototype.py
+++ prototype/IoTGateway/IoTGatewayprototype.py
@@ -15,8 +15,8 @@ s = serial.Serial("COM5", 115200)
 sensor = 1000
 
 room1DiviceId = "10e27d0"
-room1CurrentStatus = False
-room1BeforeStatus = False
+room1CurrentStatus = False　  # ドアが空いていない
+room1BeforeStatus = False　  # ドアが空いていない
 
 while 1:
     # 1行読み取る
@@ -25,15 +25,16 @@ while 1:
     m = str(data).split(";")
     if (len(m) == 13):
         if m[5] == "10e27d0":
-            #センサーの値を閾値と比較
+            # センサーの値を閾値と比較
             if int(m[9]) < sensor:
-                room1CurrentStatus = False
+                　#ドアが空いている場合
+                room1CurrentStatus = True
                 if room1BeforeStatus != room1CurrentStatus:
                     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %I:%M:%S ")
                     print(timestamp + " " + m[5] + ": is opened !")
                     # 取得した値を変数に格納
                     comp_id = 1  # ここをusbから取得した値にしたい
-                    #ドアが空いている場合、statusをYに設定
+                    # ドアが空いている場合、statusをYに設定
                     status = 'Y'  # ここをusbから取得した値にしたい
                     wc_status = {'g_id': comp_id, 'g_status': status}
                     # httpプロトコルでpost通信
@@ -41,8 +42,10 @@ while 1:
                         'http://localhost/wc_get.php', params=wc_status)
                     print(response.text)  # レスポンスのHTMLを文字列で取得
                     room1BeforeStatus = room1CurrentStatus
+
             elif int(m[9]) > sensor:
-                room1CurrentStatus = True
+                　#ドアが閉じている場合（個室が利用されている）
+                room1CurrentStatus = False　
                 if room1BeforeStatus != room1CurrentStatus:
                     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %I:%M:%S")
                     print(timestamp + " " + m[5] + ": is closed !")
