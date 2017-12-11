7a5d4679fddd4493188f054fa3a8d32d3200bb24
tatsutatsugo <doubledragontaz@gmail.com>
2017-12-11 23:37:15 +0900

    modify http post address

--- prototype/IoTGateway/IoTGatewayprototype.py
+++ prototype/IoTGateway/IoTGatewayprototype.py
@@ -15,8 +15,8 @@ s = serial.Serial("COM5", 115200)
 sensor = 1000
 
 room1DiviceId = "10e27d0"
-room1CurrentStatus = False
-room1BeforeStatus = False
+room1CurrentStatus = False　#個室が利用できるかどうか
+room1BeforeStatus = False　#以前の状態を記録
 
 while 1:
     # 1行読み取る
@@ -26,8 +26,8 @@ while 1:
     if (len(m) == 13):
         if m[5] == "10e27d0":
             #センサーの値を閾値と比較
-            if int(m[9]) < sensor:
-                room1CurrentStatus = False
+            if int(m[9]) < sensor:　#ドアが空いている場合
+                room1CurrentStatus = True
                 if room1BeforeStatus != room1CurrentStatus:
                     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %I:%M:%S ")
                     print(timestamp + " " + m[5] + ": is opened !")
@@ -38,11 +38,12 @@ while 1:
                     wc_status = {'g_id': comp_id, 'g_status': status}
                     # httpプロトコルでpost通信
                     response = requests.post(
-                        'http://localhost/wc_get.php', params=wc_status)
+                        'http://localhost/api/compartment/insert_status.php', params=wc_status)
                     print(response.text)  # レスポンスのHTMLを文字列で取得
                     room1BeforeStatus = room1CurrentStatus
-            elif int(m[9]) > sensor:
-                room1CurrentStatus = True
+
+            elif int(m[9]) > sensor:　#ドアが閉じている場合（個室が利用されている）
+                room1CurrentStatus = False　
                 if room1BeforeStatus != room1CurrentStatus:
                     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %I:%M:%S")
                     print(timestamp + " " + m[5] + ": is closed !")
@@ -52,7 +53,7 @@ while 1:
                     wc_status = {'g_id': comp_id, 'g_status': status}
                     # httpプロトコルでpost通信
                     response = requests.post(
-                        'http://localhost/wc_get.php', params=wc_status)
+                        'http://localhost/api/compartment/insert_status.php', params=wc_status)
                     print(response.text)  # レスポンスのHTMLを文字列で取得
                     room1BeforeStatus = room1CurrentStatus
         #print ("送信元シリアル番号："+m[5]+" / 送信元電源電圧："+m[6]+" / センサー電流："+m[9])
