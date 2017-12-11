16fd5ebc403aedc90bd14351d4559d7901733edf
tatsutatsugo <doubledragontaz@gmail.com>
2017-12-11 23:19:22 +0900

    beautify file

--- prototype/html/api/compartment/insert_status.php
+++ prototype/html/api/compartment/insert_status.php
@@ -3,32 +3,27 @@
 require_once '../config/database.php';
 require_once '../objects/compartment.php';
 
-if ($_SERVER["REQUEST_METHOD"]==="POST"){
- if (!empty($_POST['g_id']) && !empty($_POST['g_status'])) {
+if ($_SERVER["REQUEST_METHOD"]==="POST") {
+    if (!empty($_POST['g_id']) && !empty($_POST['g_status'])) {
 
    //POSTメソッドでセンサーから送信された値を取得し、変数に格納
-    $comp_id = htmlspecialchars($_POST['g_id'], ENT_QUOTES, 'UTF-8');
-    $status = htmlspecialchars($_POST['g_status'], ENT_QUOTES, 'UTF-8');
+        $comp_id = htmlspecialchars($_POST['g_id'], ENT_QUOTES, 'UTF-8');
+        $status = htmlspecialchars($_POST['g_status'], ENT_QUOTES, 'UTF-8');
 
-    // Databaseインスタンス生成
-    $database = new Database();
-    $db = $database->db_connect();
+        // Databaseインスタンス生成
+        $database = new Database();
+        $db = $database->db_connect();
 
-    // 初期化
-    $compartment = new compartment($db);
+        // 初期化
+        $compartment = new compartment($db);
 
-    //get_statクラスのinsert関数をセンサーの値を引数にして実行
-    $compartment->insert($comp_id,$status);
-
-
-  } else {
-    $err = "入力されていない項目があります。";
-    echo $err;
-  }
+        //get_statクラスのinsert関数をセンサーの値を引数にして実行
+        $compartment->insert($comp_id, $status);
+    } else {
+        $err = "入力されていない項目があります。";
+        echo $err;
+    }
+} else {
+    # code...
 }
-else {
-  # code...
-}
-
-
 ?>
