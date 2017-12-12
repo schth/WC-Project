ca2e043c5a735ab94329d54fecea3d94a1c8d8b6
tatsutatsugo <doubledragontaz@gmail.com>
2017-12-13 00:17:06 +0900

    merge create statement as one file

--- prototype/Database/CreateUser.sql
+++ prototype/Database/CreateUser.sql
@@ -1,4 +1,10 @@
-CREATE USER
-'wc-admin'@'localhost' IDENTIFIED BY 'wc-admin',
-'wcusr'@'localhost' IDENTIFIED BY 'wcusr',
-'wcses'@'localhost' IDENTIFIED BY 'wcses';
+SET GLOBAL validate_password_length=4;
+SET GLOBAL validate_password_policy=LOW;
+
+CREATE USER 'wc-admin'@'localhost' IDENTIFIED BY 'wc-admin';
+CREATE USER 'wcusr'@'localhost' IDENTIFIED BY 'wcusr';
+CREATE USER 'wcses'@'localhost' IDENTIFIED BY 'wcses';
+
+GRANT ALL ON `wc`.compartment TO 'wc-admin'@'localhost';
+GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.compartment TO 'wcusr'@'localhost';
+GRANT SELECT ON `wc`.compartment TO 'wcses'@'localhost';
--- prototype/Database/Create_DB_Schema.sql
+++ prototype/Database/Create_DB_Schema.sql
@@ -1,5 +1,9 @@
-CREATE TABLE compartment (
-  seq_id SERIAL,    
+CREATE DATABASE IF NOT EXISTS wc;
+
+USE wc;
+
+CREATE TABLE IF NOT EXISTS compartment (
+  seq_id SERIAL,
   comp_id integer(1),
   status varchar(1),
   upd_dt DATETIME,
