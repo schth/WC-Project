1ae24a06da266dd13eedf75927c1e69cbe4c7e9a
tatsutatsugo <doubledragontaz@gmail.com>
2017-12-18 22:54:40 +0900

    add view

--- prototype/Database/create_db_schema.sql
+++ prototype/Database/create_db_schema.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS compartment (
   upd_dt DATETIME,
   PRIMARY KEY (comp_id,
                upd_dt));
+
+CREATE VIEW visit AS
+SELECT a.seq_id,
+          a.comp_id,
+          a.status,
+          a.upd_dt AS time1,
+     (SELECT b.upd_dt
+      FROM compartment b
+      WHERE b.comp_id=a.comp_id
+        AND b.seq_id > a.seq_id
+      ORDER BY b.seq_id
+      LIMIT 1) AS time2
+   FROM compartment a
+   ORDER BY seq_id;
