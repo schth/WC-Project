8a7b9a78d12ece737fe4e4822da3988940c9efe9
tatsutatsugo <doubledragontaz@gmail.com>
2017-12-28 01:04:35 +0900

    バッククォートを削除

--- prototype/Database/create_user.sql
+++ prototype/Database/create_user.sql
@@ -5,11 +5,11 @@ CREATE USER 'wc-admin'@'localhost' IDENTIFIED BY 'wc-admin';
 CREATE USER 'wcusr'@'localhost' IDENTIFIED BY 'wcusr';
 CREATE USER 'wcses'@'localhost' IDENTIFIED BY 'wcses';
 
-GRANT ALL ON `wc`.events TO 'wc-admin'@'localhost';
-GRANT ALL ON `wc`.sensor_comp TO 'wc-admin'@'localhost';
+GRANT ALL ON wc.events TO 'wc-admin'@'localhost';
+GRANT ALL ON wc.sensor_comp TO 'wc-admin'@'localhost';
 
-GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.events TO 'wcusr'@'localhost';
-GRANT SELECT,UPDATE,INSERT,DELETE ON `wc`.sensor_comp TO 'wcusr'@'localhost';
+GRANT SELECT,UPDATE,INSERT,DELETE ON wc.events TO 'wcusr'@'localhost';
+GRANT SELECT,UPDATE,INSERT,DELETE ON wc.sensor_comp TO 'wcusr'@'localhost';
 
-GRANT SELECT ON `wc`.events TO 'wcses'@'localhost';
-GRANT SELECT ON `wc`.sensor_comp TO 'wcses'@'localhost';
+GRANT SELECT ON wc.events TO 'wcses'@'localhost';
+GRANT SELECT ON wc.sensor_comp TO 'wcses'@'localhost';
--- prototype/Database/insert_dumpdata.sql
+++ prototype/Database/insert_dumpdata.sql
@@ -1,30 +1,30 @@
 -- マスターデータ
 -- 2階男子トイレ1番目の個室
-INSERT INTO `sensor_comp`(`sensor_id`, `floor`, `gender`,`comp_id`)VALUES ('10e27d0', 2, 1, 1);
-INSERT INTO `sensor_comp`(`sensor_id`, `floor`, `gender`,`comp_id`)VALUES ('10e3533', 2, 1, 2);
-INSERT INTO `sensor_comp`(`sensor_id`, `floor`, `gender`,`comp_id`)VALUES ('10e29b1', 2, 1, 3);
-INSERT INTO `sensor_comp`(`sensor_id`, `floor`, `gender`,`comp_id`)VALUES ('10e34ee', 2, 1, 4);
+INSERT INTO sensor_comp(sensor_id, floor, gender,comp_id)VALUES ('10e27d0', 2, 1, 1);
+INSERT INTO sensor_comp(sensor_id, floor, gender,comp_id)VALUES ('10e3533', 2, 1, 2);
+INSERT INTO sensor_comp(sensor_id, floor, gender,comp_id)VALUES ('10e29b1', 2, 1, 3);
+INSERT INTO sensor_comp(sensor_id, floor, gender,comp_id)VALUES ('10e34ee', 2, 1, 4);
 -- トランザクションデータ
 
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e27d0','N','2017-11-11 12:00:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e3533','N','2017-11-11 12:00:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e29b1','N','2017-11-11 12:00:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e34ee','N','2017-11-11 12:00:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e27d0','Y','2017-11-11 12:10:10');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e3533','Y','2017-11-11 12:20:20');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e27d0','N','2017-11-11 12:20:30');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e29b1','Y','2017-11-11 12:30:30');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e3533','N','2017-11-11 12:30:40');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e34ee','Y','2017-11-11 12:40:40');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e29b1','N','2017-11-11 12:40:50');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e34ee','N','2017-11-11 12:50:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e27d0','Y','2017-11-11 13:40:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e3533','Y','2017-11-11 13:50:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e29b1','Y','2017-11-11 14:10:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e34ee','Y','2017-11-11 14:20:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e27d0','N','2017-11-11 14:40:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e3533','N','2017-11-11 14:50:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e29b1','N','2017-11-11 15:10:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e27d0','Y','2017-11-11 15:15:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e34ee','N','2017-11-11 15:20:00');
-INSERT INTO `events`(`sensor_id`, `Status`, `upd_dt`)VALUES ('10e29b1','Y','2017-11-11 15:25:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e27d0','N','2017-11-11 12:00:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e3533','N','2017-11-11 12:00:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e29b1','N','2017-11-11 12:00:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e34ee','N','2017-11-11 12:00:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e27d0','Y','2017-11-11 12:10:10');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e3533','Y','2017-11-11 12:20:20');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e27d0','N','2017-11-11 12:20:30');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e29b1','Y','2017-11-11 12:30:30');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e3533','N','2017-11-11 12:30:40');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e34ee','Y','2017-11-11 12:40:40');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e29b1','N','2017-11-11 12:40:50');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e34ee','N','2017-11-11 12:50:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e27d0','Y','2017-11-11 13:40:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e3533','Y','2017-11-11 13:50:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e29b1','Y','2017-11-11 14:10:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e34ee','Y','2017-11-11 14:20:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e27d0','N','2017-11-11 14:40:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e3533','N','2017-11-11 14:50:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e29b1','N','2017-11-11 15:10:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e27d0','Y','2017-11-11 15:15:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e34ee','N','2017-11-11 15:20:00');
+INSERT INTO events(sensor_id, status, upd_dt)VALUES ('10e29b1','Y','2017-11-11 15:25:00');
